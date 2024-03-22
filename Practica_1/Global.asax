241fe72bf3f01aafdde594632d94a6fa4daaaa1c
Fabiola <Fabiola@LAPTOP-5NFTL38M>
2024-03-21 18:17:41 -0700

    Add project files.

---
+++ Practica_1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Practica_1.MvcApplication" Language="C#" %>
---
+++ Practica_1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Practica_1.Startup))]
+namespace Practica_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
